9629ec59501bf5e3a471a16991a44488f4bc7620
Joseph Palmezano <joseph.palmezano@wizeline.com>
2021-11-23 10:16:37 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../src/components/Header/Header.js'
+import Footer from '../src/components/Footer/Footer.js'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const FooterWrapper = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  background-color: #263238;
+  text-align: center;
+  color: white;
+`;
+
+const Footer = () => (
+  <FooterWrapper>Ecommerce created during Wizeline’s Academy React Bootcamp</FooterWrapper>
+);
+
+export default Footer;
