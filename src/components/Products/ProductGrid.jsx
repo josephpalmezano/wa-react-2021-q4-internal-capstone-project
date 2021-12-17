fe53f95774715a67d44080506598737afda6e397
Joseph Palmezano <joseph.palmezano@wizeline.com>
2021-12-17 16:27:41 -0500

    view all products button

--- src/components/Products/ProductGrid.jsx
+++ src/components/Products/ProductGrid.jsx
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 import "./ProductGrid.css";
 import ProductThumb  from "./ProductThumb";
 import FeaturedProducts from "../../mocks/en-us/featured-products.json";
+import { Link } from "react-router-dom";
 
 function ProductGrid() {
   const [data] = useState(FeaturedProducts.results);
 
   return (
-    <div className="thumb-wrapper">
-      <ProductThumb data={data} />
-    </div>
+    <>
+      <div className="thumb-wrapper">
+        <ProductThumb data={data} />
+      </div>
+      <div className="btn-wrapper">
+        <Link className="all-products-btn" to="products">VIEW ALL PRODUCTS</Link>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Products/ProductsList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ProductsList.css";
+
+function ProductsList() {
+  return (
+    <>
+      <h1>PRODUCTS HERE</h1>
+    </>
+  );
+}
+
+export default ProductsList;
