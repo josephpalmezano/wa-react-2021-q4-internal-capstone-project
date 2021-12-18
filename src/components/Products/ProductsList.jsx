fcac2619ca81773479481fa6cfb97ff63b13c8d6
Joseph Palmezano <joseph.palmezano@wizeline.com>
2021-12-17 19:41:33 -0500

    filter products by category

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ProductsList.css";
+import ProductThumb  from "./ProductThumb";
+import Sidebar from "../Sidebar/Sidebar";
+import Products from "../../mocks/en-us/products.json";
 
 function ProductsList() {
+  let [selectedCategory, setSelectedCategory] = useState("")
+  let [data] = useState(Products.results);
+  let [products, setProducts] = useState([]);
+  useEffect(() => {
+    setProducts(selectedCategory.length > 0 ? data.filter(d => d.data.category.id === selectedCategory) : data);
+  },[data, selectedCategory]);
+
   return (
     <>
-      <h1>PRODUCTS HERE</h1>
+      <main>
+        <Sidebar setSelectedCategory={setSelectedCategory}/>
+        <div className="thumb-wrapper">
+          <ProductThumb data={products} />
+        </div>
+      </main>
     </>
   );
 }
